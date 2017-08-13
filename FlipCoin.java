00fde3c80b57456584ef59fd1882957d9e9806ed
Rohit Shukla <30771705+rds7@users.noreply.github.com>
2017-08-13 14:41:37 +0530

    13/08 commit

--- FlipCoin.java
+++ FlipCoin.java
@@ -12,23 +12,39 @@
  *  @version 1.0
  *  @since   06-08-2017
  * output:
- *  HEADS
- *  TAILS
+ * Enter how many times you want to flip a coin:
+ *	9
+ *	Number of Heads:2
+ *	Number of Tails:7
+ *	Percentage of Heads:22%
+ *	Percentage of Tails:77%
  ******************************************************************************/
 import java.util.Scanner;
 public class FlipCoin
 {
 	public static void main(String [] args)
 	{
-		int c;
+		int c,n,head=0,tail=0;
+		System.out.println("Enter how many times you want to flip a coin:");
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		for(int i=1;i<=n;i++)
+		{
 		c=(int)(Math.random()*2)+1;
 		if(c==1)
 		{
-			System.out.println("HEADS");
+			head++;                        // h is counter variable to count the heads
+			
 		}
 		else
 		{
-			System.out.println("TAILS");
+			
+			tail++;						// t is counter variable to count the tails
 		}
+		}
+		System.out.println("Number of Heads:"+head);
+		System.out.println("Number of Tails:"+tail);
+		System.out.println("Percentage of Heads:"+(head*100)/n+"%");
+		System.out.println("Percentage of Tails:"+(tail*100)/n+"%");
 	}//main end
 }//lass end
